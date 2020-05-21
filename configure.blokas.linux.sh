80d28a402b71fbfbdf894d8d53599d3fb666d09b
Giedrius <giedrius@blokas.io>
2020-05-21 10:21:59 +0300

    Enable support for some image formats on Linux, so icons in file browser dialog get rendered

--- configure.blokas.linux.sh
+++ configure.blokas.linux.sh
@@ -9,7 +9,7 @@ ${QTDIR}/configure -prefix ${PREFIX} -opensource -confirm-license -nomake tests 
 	-skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland \
 	-skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwebview -skip qtxmlpatterns -no-feature-textodfwriter -no-feature-sharedmemory \
 	-no-feature-systemsemaphore -no-feature-im -no-feature-process -no-feature-dom -no-feature-printpreviewwidget -no-feature-printpreviewdialog \
-	-no-feature-cups -no-feature-fontdialog -no-feature-datawidgetmapper -no-feature-imageformat_bmp -no-feature-imageformat_ppm -no-feature-imageformat_xbm \
-	-no-feature-imageformat_png -no-feature-imageformat_jpeg -no-feature-image_text -no-feature-colornames -no-feature-cups -no-feature-paint_debug \
+	-no-feature-cups -no-feature-fontdialog -no-feature-datawidgetmapper -no-feature-imageformat_bmp -no-feature-imageformat_ppm \
+	-no-feature-imageformat_jpeg -no-feature-image_text -no-feature-colornames -no-feature-cups -no-feature-paint_debug \
 	-no-feature-codecs -no-feature-big_codecs -no-feature-iconv -no-feature-ftp -no-feature-udpsocket -no-feature-networkproxy -no-feature-socks5 -no-feature-networkdiskcache \
 	-no-feature-bearermanagement -skip qttools
